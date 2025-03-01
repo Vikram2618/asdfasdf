e3952df2ba6cecbe7928e01fa783287a18884975
Vikram Rathod <vikram932131@gmail.com>
2025-03-01 15:15:54 +0530

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const DbConnection = () =>{
-    mongoose.connect('mongodb+srv://vikram:dkdjdjkfdkedjdkk@cluster0.iddoh.mongodb.net/codebugbackend').then(()=>{
+    mongoose.connect(process.env.MONGODB_URL).then(()=>{
         console.log('mongodb is connected');
     }).catch((err)=>{
         console.log('A Error is appear : ' + err);
